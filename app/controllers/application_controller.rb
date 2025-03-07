18f834db34ca19f0b7c1316e0f817b3d74aa1b4c
mipei02091218 <emyutankiiroina31@gmail.com>
2025-03-07 21:45:36 +0900

    deviseに関するviewファイルの作成とユーザー認証機能の実装 2
    -viewファイルの設置の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if::devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -14,5 +14,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
-  
+
 end
