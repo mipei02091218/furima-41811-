35ae4a454c6ec71e298c4cee0c30e26c49674fec
mipei02091218 <emyutankiiroina31@gmail.com>
2025-03-19 14:35:11 +0900

    商品情報の編集機能　修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,8 +50,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return if current_user.id == @item.user_id
-
-    redirect_to root_path
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
