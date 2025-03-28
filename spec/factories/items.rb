cf366f2fe464b1dd31c83a74758fc2ea5df8d17c
mipei02091218 <emyutankiiroina31@gmail.com>
2025-03-28 17:01:01 +0900

    prefecture_idの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     price           { Faker::Number.between(from: 300, to: 9_999_999) }
     category_id     { Faker::Number.between(from: 2, to: 11) }
     condition_id    { Faker::Number.between(from: 2, to: 7) }
-    prefecture_id   { Faker::Number.between(from: 2, to: 47) }
+    prefecture_id   { Faker::Number.between(from: 2, to: 48) }
     shipping_fee_id { Faker::Number.between(from: 2, to: 3) }
     shipping_day_id { Faker::Number.between(from: 2, to: 4) }
 
--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order_form do
     postal_code   { Faker::Number.number(digits: 3).to_s + '-' + Faker::Number.number(digits: 4).to_s }
-    prefecture_id { Faker::Number.between(from: 2, to: 47) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
     city          { Faker::Address.city }
     street        { Faker::Address.street_name }
     building      { Faker::Address.secondary_address }
